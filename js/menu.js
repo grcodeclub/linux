ae17233dd3b82889150abb084b1bf56784594998
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-19 13:44:50 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,13 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/linux/">Linux</li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/system/">Λειτουργικό Σύστημα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/linux/terminal">Terminal</li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/linux/post/install">Εγκατάσταση Προγραμμάτων</li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/linux/post/system_file">Αρχεία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-     menuLink = '<li><a href="https://grcodeclub.github.io/en/system/linux">English</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/system">Λειτουργικά Συστήματα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
