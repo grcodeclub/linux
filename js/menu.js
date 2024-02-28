0a1ea3af2d1b25612467e9ed0222ea012e2f3479
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-28 23:27:31 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/linux/terminal">Terminal</li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/linux/post/install">Εγκατάσταση Προγραμμάτων</li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/linux/install">Εγκατάσταση Προγραμμάτων</li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/linux/post/system_file">Αρχεία</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/linux/system_file">Αρχεία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/system">Λειτουργικά Συστήματα</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.gr/system">Λειτουργικά Συστήματα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
